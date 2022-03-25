1c428d470ac05b60e2539ec3f6743f7b35acaf54
DreamyTZK <dreamytzk@outlook.com>
2022-03-25 23:09:02 +0800

    ✨ feat: 添加feed字段用于指定友链爬虫的域名

--- src/plugins/fcircle/fcircle.js
+++ src/plugins/fcircle/fcircle.js
@@ -1,13 +1,17 @@
 /**
  * 生成用于https://github.com/Rock-Candy-Tea/hexo-circle-of-friends项目的json格式数据
- * @param friendList {{avatar:String,description:String,link:String,name:String,subSuffix?:String,banSub?:Boolean}[]} 友链列表
+ * @param friendList {{avatar:String,description:String,link:String,feed:String,name:String,subSuffix?:String,banSub?:Boolean}[]} 友链列表
  * @returns
  */
 function index(friendList) {
   const result = []
   friendList.forEach((friend) => {
     if (!friend.banSub) {
-      const friendInfo = [friend.name, friend.link, friend.avatar]
+      const friendInfo = [
+        friend.name,
+        friend.feed || friend.link,
+        friend.avatar
+      ]
       if (friend.subSuffix) {
         friendInfo.push(friend.subSuffix)
       }
